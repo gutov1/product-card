7d6ee8ab90d7960303703b36f4380d0d330fc933
gutov1 <amirgutov3@gmail.com>
2026-08-12 21:07:01 +0300

    fixing a bug with the overlay

---
+++ homework-14.js
@@ -0,0 +1,49 @@
+export class Modal {
+  constructor(modalId, buttonId, shouldCloseOnOverlay) {
+    this.modal = document.getElementById(modalId);
+    this.overlay = document.getElementById('overlay');
+    this.#initOpen(buttonId);
+    this.#initClose(shouldCloseOnOverlay);
+    this.shouldCloseOnOverlay = shouldCloseOnOverlay;
+    
+    this.overlayClickHandler = () => {
+      this.close();
+    }
+  }
+
+  open() {
+    this.modal.classList.add('modal-showed');
+    this.overlay.classList.add('overlay-showed');
+
+    if (this.shouldCloseOnOverlay) {
+      this.overlay.addEventListener('click', this.overlayClickHandler);
+    }
+  }
+
+  close() {
+    this.modal.classList.remove('modal-showed');
+    this.overlay.classList.remove('overlay-showed');
+
+    if (this.shouldCloseOnOverlay) {
+      this.overlay.removeEventListener('click', this.overlayClickHandler);
+    }
+  }
+
+  isOpen() {
+    return this.modal.classList.contains('modal-showed');
+  }
+
+  #initOpen(buttonId) {
+    const button = document.getElementById(buttonId);
+    button.addEventListener('click', () => {
+      this.open();
+    })
+  }
+
+  #initClose() {
+    const closeButton = this.modal.querySelector('#modal-close-button');
+    closeButton.addEventListener('click', () => {
+      this.close();
+    });
+  }
+}
