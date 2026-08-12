c8228181581843b53a88d9e3f9a02c4be92408eb
gutov1 <amirgutov3@gmail.com>
2026-08-12 11:24:45 +0300

    implement OOP cafe system with Drink, Cafe classes and inheritance

--- homework-13.js
+++ homework-13.js
@@ -30,3 +30,69 @@ class Drink {
     return this.getInfo();
   }
 }
+
+class Tea extends Drink {
+  constructor(name, size, price, temperature, teaType) {
+    super(name, size, price, temperature);
+    this.teaType = teaType;
+  }
+
+  getInfo() {
+    return `${super.getInfo()}, сорт: ${this.teaType}`;
+  }
+}
+
+class Coffee extends Drink {
+  constructor(name, size, price, temperature, beanType, milkType) {
+    super(name, size, price, temperature);
+    this.beanType = beanType;
+    this.milkType = milkType;
+  }
+
+  getInfo() {
+    const baseInfo = super.getInfo();
+    return `${baseInfo}, зёрна: ${this.beanType}, молоко: ${this.milkType}`;
+  }  
+}
+
+class Lemonade extends Drink {
+  constructor(name, size, price, temperature, fruitFlavor) {
+    super(name, size, price, temperature);
+    this.fruitFlavor = fruitFlavor;
+  }
+
+  getInfo() {
+    return `${super.getInfo()}, вкус: ${this.fruitFlavor}`;
+  }
+}
+
+class Cafe {
+  constructor(name, location) {
+    this.name = name;
+    this.location = location;
+  }
+
+  getInfo() {
+    return `Кафе "${this.name}", расположено: ${this.location}`;
+  }
+
+  orderDrink(drink) {
+    console.log(`Клиент заказал: ${drink.name}`);
+    const result = drink.serve();
+    console.log(`Заказ обработан кафе "${this.name}"`);
+    return result;
+  }
+}
+
+const cafe = new Cafe("Coffee Point", "ул. Пушкина, 10");
+console.log(cafe.getInfo());
+
+const latte = new Coffee("Латте", "M", 250, 70, "арабика", "цельное");
+const greenTea = new Tea("Зелёный чай", "L", 150, 85, "сенча");
+const lemonade = new Lemonade("Лимонад", "L", 180, 5, "лимон-мята");
+
+console.log(cafe.orderDrink(latte));
+
+console.log(latte.getTemperature());
+latte.setTemperature(65);
+console.log(latte.getTemperature());
